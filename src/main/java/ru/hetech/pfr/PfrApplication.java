4c63dc7f3bfc0bf3a2d9689d5b6f26922297246f
AFedorov <aleksey.g.fedorov@gmail.com>
2017-05-15 07:13:46 +0300

    Init

---
+++ src/main/java/ru/hetech/pfr/PfrApplication.java
@@ -0,0 +1,12 @@
+package ru.hetech.pfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfrApplication.class, args);
+	}
+}
---
+++ src/main/java/ru/hetech/pfr/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package ru.hetech.pfr.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ru.hetech.pfr.model.Role;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+	Role findByRole(String role);
+
+}
